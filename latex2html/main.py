034c6528bb4f9d0e1ccf0341693b7348fb3598ec
Ivar Clemens <post@ivarclemens.nl>
2015-07-09 16:14:47 +0100

    Updated HTML generator.

--- latex2html/main.py
+++ latex2html/main.py
@@ -9,7 +9,7 @@
   # Headings
   # Labels
   # Citations
-  
+
 # Number chapters, graphics, equations, and tables
 # Sort and resolve citations
 
@@ -39,9 +39,9 @@ def inputFile(match):
 	buffer = ""
 	for line in infile:
 		buffer += line
-	
+
 	return resolveInputs(buffer)
-	
+
 def resolveInputs(source):
 	return re.sub("\\\\input{(.*)}", inputFile, source, flags = re.MULTILINE)
 
@@ -57,12 +57,19 @@ source = re.sub("\\\\%", "__PERCENT__", source)
 source = re.sub("%(.*?)\n", "", source)
 source = re.sub("__PERCENT__", "%", source)
 
+source = re.sub("\\\\\"i", "&iuml;", source)
+source = re.sub("\\\\\"e", "&euml;", source)
+source = re.sub("\\\\\'e", "&eacute;", source)
 
 #
 # Replace all headings
 #
 
 namerefs = dict();
+
+def chapterHTMLGroup(match):
+	namerefs[match.group(2)] = match.group(1)
+	return "<h1 name=\"" + match.group(2) + "\">" + match.group(1) + "</h1>"
 
 def chapterHTML(match):
 	return "<h1>" + match.group(1) + "</h1>"
@@ -80,27 +87,29 @@ def subsectionHTMLGroup(match):
 
 def subsectionHTML(match):
 	return "<h3>" + match.group(1) + "</h3>"
-	
+
 def subsubsectionHTMLGroup(match):
 	namerefs[match.group(2)] = match.group(1)
 	return "<h4 name=\"" + match.group(2) + "\">" + match.group(1) + "</h4>"
 
 def subsubsectionHTML(match):
 	return "<h4>" + match.group(1) + "</h4>"
-	
+
 def resolveChapters(source):
+	source = re.sub("\\\\chapter\*{([^}]*)}", chapterHTML, source, flags = re.MULTILINE)
+	source = re.sub("\\\\chapter{([^}]*)}\s*\\\\chaptermark{}\s*\\\\label{([^}]*)}", chapterHTMLGroup, source, flags = re.MULTILINE)
 	source = re.sub("\\\\chapter{([^}]*)}", chapterHTML, source, flags = re.MULTILINE)
 	source = re.sub("\\\\section{([^}]*)}\s*\\\\label{([^}]*)}", sectionHTMLGroup, source, flags = re.MULTILINE)
 	source = re.sub("\\\\section{([^}]*)}", sectionHTML, source, flags = re.MULTILINE)
-	
+
 	source = re.sub("\\\\subsection{([^}]*)}\s*\\\\label{([^}]*)}", subsectionHTMLGroup, source, flags = re.MULTILINE)
 	source = re.sub("\\\\subsection{([^}]*)}", subsectionHTML, source, flags = re.MULTILINE)
 
 	source = re.sub("\\\\subsubsection{([^}]*)}\s*\\\\label{([^}]*)}", subsubsectionHTMLGroup, source, flags = re.MULTILINE)
 	source = re.sub("\\\\subsubsection{([^}]*)}", subsubsectionHTML, source, flags = re.MULTILINE)
-	
+
 	return source
-	
+
 source = resolveChapters(source)
 
 
@@ -111,27 +120,27 @@ source = resolveChapters(source)
 eqns = dict()
 eqnc = [0]
 
-def equationHTML(match):	
+def equationHTML(match):
 	tmp = re.search("\\\\label{(.*?)}", match.group(1))
-	
+
 	if tmp is None:
 		label = "No label"
 	else:
 		label = tmp.group(1)
-	
-	eqnc[0] = eqnc[0] + 1 
+
+	eqnc[0] = eqnc[0] + 1
 	eqns[label] = str(eqnc[0])
-	
+
 	equation = re.sub("\\\\label{(.*?)}", "", match.group(1))
 	#return "\[" + equation + "\]<p><b>Equation " + str(eqnc[0]) + "</b></p>"
 	return "<p style='font-weight: bold'>Equation " + str(eqnc[0]) + " about here</p>"
-	
+
 
 def resolveEquations(source):
 	source = re.sub("\\\\begin{equation}([\s\S]*?)\\\\end{equation}", equationHTML, source, flags = re.MULTILINE)
 	return source
 
-	
+
 source = resolveEquations(source)
 
 
@@ -144,15 +153,15 @@ figc = [0]
 
 def figureHTML(match):
 	tmp = re.search("\\\\label{(.*?)}", match.group(1))
-	
+
 	if tmp is None:
 		label = "No label"
 	else:
 		label = tmp.group(1)
-	
-	figc[0] = figc[0] + 1 
+
+	figc[0] = figc[0] + 1
 	figs[label] = str(figc[0])
-	
+
 	return "<p style='font-weight: bold'>Figure " + str(figc[0]) + " about here</p>"
 
 def resolveFigures(source):
@@ -171,15 +180,15 @@ tabc = [0]
 
 def tableHTML(match):
 	tmp = re.search("\\\\label{(.*?)}", match.group(1))
-	
+
 	if tmp is None:
 		label = "No label"
 	else:
 		label = tmp.group(1)
-	
-	tabc[0] = tabc[0] + 1 
+
+	tabc[0] = tabc[0] + 1
 	tabs[label] = str(tabc[0])
-	
+
 	return "<p style='font-weight: bold'>Table " + str(tabc[0]) + " about here</p>"
 
 def resolveTables(source):
@@ -201,28 +210,61 @@ def nameRefHTML(match):
 def eqnRefHTML(match):
 	if not match.group(1) in eqns:
 		return "<a href=\"#\">Undefined equation label</a>"
-	return "<a href=\"#" + match.group(1) + "\">Equation " + eqns[match.group(1)] + "</a>"	
+	return "<a href=\"#" + match.group(1) + "\">Equation " + eqns[match.group(1)] + "</a>"
 
 def figRefHTML(match):
 	if not match.group(1) in figs:
 		return "<a href=\"#\">Undefined figure label</a>"
-	return "<a href=\"#" + match.group(1) + "\">Figure " + figs[match.group(1)] + "</a>"	
+	return "<a href=\"#" + match.group(1) + "\">Figure " + figs[match.group(1)] + "</a>"
 
 def tabRefHTML(match):
 	if not match.group(1) in tabs:
 		return "<a href=\"#\">Undefined table label</a>"
-	return "<a href=\"#" + match.group(1) + "\">Table " + tabs[match.group(1)] + "</a>"	
+	return "<a href=\"#" + match.group(1) + "\">Table " + tabs[match.group(1)] + "</a>"
 
-	
+def hoofdstukRefHTML(match):
+	if not match.group(1) in namerefs:
+		return "<a href=\"#\">Undefined chapter label</a>"
+	return "<a href=\"#" + match.group(1) + "\">Hoofdstuk " + namerefs[match.group(1)] + "</a>"
+
 def resolveRefs(source):
 	source = re.sub("\\\\nameref{([^}]*?)}", nameRefHTML, source, flags = re.MULTILINE)
 	source = re.sub("\\\\eqnref{([^}]*?)}", eqnRefHTML, source, flags = re.MULTILINE)
 	source = re.sub("\\\\figref{([^}]*?)}", figRefHTML, source, flags = re.MULTILINE)
 	source = re.sub("\\\\tabref{([^}]*?)}", tabRefHTML, source, flags = re.MULTILINE)
+
+	source = re.sub("\\\\Hoofdstuk{([^}]*?)}", hoofdstukRefHTML, source, flags = re.MULTILINE)
+
 	return source
 
 source = resolveRefs(source)
 
+def descriptionItemHTML(match):
+	code = "<li>"
+	code += "<b>" + match.group(1) + "</b>: " + match.group(2)
+	code += "</li>"
+	return code
+
+def descriptionHTML(match):
+	code = "<ul>"
+	code += re.sub("\\\\item\[(.*?)\](.*)", descriptionItemHTML, match.group(1), flags = re.MULTILINE)
+	code += "</ul>"
+	return code
+
+def resolveDescription(source):
+	source = re.sub("\\\\begin{description}([\s\S]*?)\\\\end{description}", descriptionHTML, source, flags = re.MULTILINE)
+	return source
+
+source = resolveDescription(source)
+
+
+def citationHTML(match):
+	return "(<a href=\"#\">" + match.group(1) + "</a>)"
+
+def resolveCitations(source):
+	return re.sub("\\\\cite{([^}]*)}", citationHTML, source, flags = re.MULTILINE)
+
+source = resolveCitations(source)
 
 
 title = ""
@@ -232,9 +274,9 @@ print """
 			<html>
 				<head>
 					<title>""" + title + """</title>
-	
+
 					<meta charset="utf8" />
-						
+
 					<script type="text/x-mathjax-config">
 					</script>
 					<style>
@@ -247,7 +289,7 @@ print """
 
 for line in source.split("\n"):
 	line = line.strip()
-	
+
 	if len(line) == 0:
 		continue
 	c = line[0]
